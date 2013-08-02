24295093d3c507dca00b9e76821b5c2d3a12c564
Kevin Moore <kevin@thinkpixellab.com>
2013-08-02 15:19:28 -0500

    launching a packaged app all pretty like

--- tool/src/chrome.dart
+++ tool/src/chrome.dart
@@ -8,24 +8,44 @@ import 'package:bot_io/bot_io.dart';
 const _chromePath = '/Users/kevin/source/dev/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 Future launchChrome() {
-  return TempDir.then(_launchChrome);
+  var testLaunch =
+    {
+     'load-and-launch-app': '/Users/kevin/source/github/pop-pop-win/app_package',
+     'no-startup-window': null
+    };
+
+  return TempDir.then((dir) => _launchChrome(dir, testLaunch));
 }
 
-Future _launchChrome(Directory tempDir) {
-  print('rockin $tempDir');
+Future _launchChrome(Directory tempDir, [Map<String, String> argsMap]) {
 
-  var argsMap = {};
+  if(argsMap == null) {
+    argsMap = {};
+  }
   argsMap['user-data-dir'] = tempDir.path;
+  argsMap['no-default-browser-check'] = null;
+  argsMap['no-first-run'] = null;
 
   var args = argsMap.keys.map((key) {
     var value = argsMap[key];
-    return "--$key=$value";
+
+    var str = "--$key";
+    if(value == null) {
+      return str;
+    } else {
+      return "$str=$value";
+    }
   }).toList(growable: false);
+
+  print(args);
 
   return Process.run(_chromePath, args)
       .then((ProcessResult pr) {
-        print(pr.stdout);
-        print(pr.stderr);
-        print(pr.exitCode);
+
+        if(pr.exitCode != 0) {
+          print(pr.stdout);
+          print(pr.stderr);
+          throw new Exception('chrome did not exit happy');
+        }
       });
 }
