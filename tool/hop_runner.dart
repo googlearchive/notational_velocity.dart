83a8a4f8df8f382d00b1b9778c025bd8a3e4da88
Kevin Moore <kevin@thinkpixellab.com>
2013-08-05 15:08:50 -0500

    hop: copy task for javascript dependencies

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,6 +1,7 @@
 library nv.hop;
 
 import 'dart:async';
+import 'dart:io';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 import 'package:hop/src/hop_experimental.dart' as hop_ex;
@@ -24,7 +25,14 @@ void main() {
       rejectDeprecatedFeatures: true,
       allowUnsafeEval: false));
 
-  // TODO: add in a script to auto-update the packaged scripts to load
+  var map = {
+             'packages/unittest/test_controller.js': 'test/app/test_controller.js',
+             'packages/browser/dart.js': 'test/app/dart.js',
+             'packages/browser/interop.js': 'test/app/interop.js',
+             'packages/js/dart_interop.js': 'test/app/dart_interop.js',
+    };
+
+  addTask('update_js', new Task.async((TaskContext ctx) => _copyFiles(ctx, map)));
 
   runHop();
 }
@@ -33,6 +41,27 @@ Future<bool> _launchChrome(TaskContext ctx) {
   return chrome.launchChrome().then((_) => true);
 }
 
-Future _copyFile(TaskContext ctx, String sourcePath, String destinationPath) {
+Future<bool> _copyFiles(TaskContext ctx, Map<String, String> sourceToDest) {
+  return Future.forEach(sourceToDest.keys, (String sourcePath) {
+    var destPath = sourceToDest[sourcePath];
 
+    ctx.config('Checking $destPath with $sourcePath');
+
+    return _copyFile(sourcePath, destPath)
+        .then((bool success) {
+          if(success) {
+            ctx.info('$destPath updated with content from $sourcePath');
+          } else {
+            ctx.info('$destPath is the same as $sourcePath');
+          }
+        });
+  }).then((_) => true);
+}
+
+Future<bool> _copyFile(String sourcePath, String destinationPath) {
+
+  return hop_ex.transformFile(destinationPath, (String original) {
+    var source = new File(sourcePath);
+    return source.readAsString();
+  });
 }
