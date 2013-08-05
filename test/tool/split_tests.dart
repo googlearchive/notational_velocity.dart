7b6adb95d64d727ccf95a9340f97c54aceba2a57
Kevin Moore <kevin@thinkpixellab.com>
2013-08-04 21:55:04 -0500

    split working!

--- test/tool/split_tests.dart
+++ test/tool/split_tests.dart
@@ -8,60 +8,33 @@ import '../../tool/src/split.dart';
 
 void main() {
   group('split', () {
-    test('test parsing', _test);
+    test('test parsing', () {
+
+      var regexp = new RegExp(r'^#\d+', multiLine: true);
+
+      var expected = [
+                      new Section(null, """silly prefix
+
+"""),
+                      new Section('#1', """one content here with #2 to ignore
+""")
+                      ];
+
+      return _test('test/data/simple_data.txt', regexp, expected);
+    });
   });
 }
 
-Future _test() {
-  var file = new File('test/data/pride_and_prejudice.txt');
+Future _test(String inputFile, RegExp regexp, List<Section> expectedResults) {
+  var file = new File(inputFile);
   assert(file.existsSync());
 
   return file.openRead()
       .transform(new Utf8Decoder())
-      .transform(new SectionSplitter())
-      .take(2)
+      .transform(new SectionSplitter(regexp))
+      .take(expectedResults.length)
       .toList()
       .then((List<Section> sections) {
-        expect(sections, hasLength(2));
+        expect(sections, orderedEquals(expectedResults));
       });
 }
-
-//final _chromeLogRegexp = new RegExp('\[(\d+)\:(\d+)\:(\d+)/(\d+):(\w+):(.*)\]', multiLine: false);
-
-final _pAndPFirst = '''The Project Gutenberg EBook of Pride and Prejudice, by Jane Austen
-
-This eBook is for the use of anyone anywhere at no cost and with
-almost no restrictions whatsoever.  You may copy it, give it away or
-re-use it under the terms of the Project Gutenberg License included
-with this eBook or online at www.gutenberg.org
-
-
-Title: Pride and Prejudice
-
-Author: Jane Austen
-
-Posting Date: August 26, 2008 [EBook #1342]
-Release Date: June, 1998
-[Last updated: October 12, 2012]
-
-Language: English
-
-
-*** START OF THIS PROJECT GUTENBERG EBOOK PRIDE AND PREJUDICE ***
-
-
-
-
-Produced by Anonymous Volunteers
-
-
-
-
-
-PRIDE AND PREJUDICE
-
-By Jane Austen
-
-
-
-''';
--- tool/src/split.dart
+++ tool/src/split.dart
@@ -1,46 +1,57 @@
 library nv.tool.split;
 
 import 'dart:convert';
+import 'package:bot/bot.dart';
 
 class Section {
   final String header;
   final String content;
 
   Section(this.header, this.content);
+
+  int get hashCode => Util.getHashCode([header, content]);
+
+  bool operator ==(other) {
+    return other is Section && header == other.header && content == other.content;
+  }
+
+  String toString() => '"$header" - "$content"';
 }
 
 class SectionSplitter extends Converter<String, List<Section>> {
+  final Pattern pattern;
+
+  SectionSplitter(this.pattern);
+
   List<Section> convert(String data) {
     var lines = new List<Section>();
 
-    _SectionSplitterSink._addSlice(data, 0, data.length, true, lines.add);
+    _SectionSplitterSink._addSlice(pattern, data, true, lines.add);
 
     return lines;
   }
 
   ChunkedConversionSink startChunkedConversion(ChunkedConversionSink<Section> sink) {
-    return new _SectionSplitterSink(sink);
+    return new _SectionSplitterSink(pattern, sink);
   }
 }
 
 class _SectionSplitterSink extends StringConversionSinkBase {
-  static const int _LF = 10;
-  static const int _CR = 13;
-
+  final Pattern _pattern;
   final ChunkedConversionSink<Section> _sink;
 
   String _carry;
 
-  _SectionSplitterSink(this._sink);
+  _SectionSplitterSink(this._pattern, this._sink);
 
   void addSlice(String chunk, int start, int end, bool isLast) {
+
+    chunk = chunk.substring(start, end);
     if(_carry != null) {
       chunk = _carry + chunk.substring(start, end);
-      start = 0;
-      end = chunk.length;
       _carry = null;
     }
-    _carry = _addSlice(chunk, start, end, isLast, _sink.add);
+    _carry = _addSlice(_pattern, chunk, isLast, _sink.add);
     if(isLast) _sink.close();
   }
 
@@ -48,43 +59,52 @@ class _SectionSplitterSink extends StringConversionSinkBase {
     addSlice('', 0, 0, true);
   }
 
-  static String _addSlice(String chunk, int start, int end, bool isLast, void adder(Section record)) {
-    String carry = null;
-    int startPos = start;
-    int pos = start;
-    while (pos < end) {
-      int skip = 0;
-      int char = chunk.codeUnitAt(pos);
-      if (char == _LF) {
-        skip = 1;
-      } else if (char == _CR) {
-        skip = 1;
-        if (pos + 1 < end) {
-          if (chunk.codeUnitAt(pos + 1) == _LF) {
-            skip = 2;
-          }
-        } else if (!isLast) {
-          return chunk.substring(startPos, end);
-        }
-      }
-      if (skip > 0) {
-        var val = chunk.substring(startPos, pos);
+  static String _addSlice(Pattern pattern, String chunk, bool isLast,
+                          void adder(Section record)) {
+    int start = 0;
+    int lastStart = 0;
+    var match = _getFirstMatch(pattern, chunk, start);
 
-        adder(new Section(null, val));
-        startPos = pos = pos + skip;
-      } else {
-        pos++;
+    String header = null;
+    String content = null;
+
+    while(match != null) {
+      content = chunk.substring(start, match.start);
+
+      if(content.isNotEmpty) {
+        var section = new Section(header, content);
+        adder(section);
+        content = null;
       }
+
+      header = chunk.substring(match.start, match.end);
+
+      lastStart = match.start;
+      start = match.end;
+      match = _getFirstMatch(pattern, chunk, start);
     }
-    if (pos != startPos) {
-      // Add remaining
-      carry = chunk.substring(startPos, pos);
+
+    if(isLast) {
+      var section = new Section(header, chunk.substring(start));
+      adder(section);
+      return null;
+    } else {
+      var carry = chunk.substring(lastStart);
+      return carry;
     }
-    if(isLast && carry != null) {
-      adder(new Section(null, carry));
+  }
+
+  static Match _getFirstMatch(Pattern pattern, String str, int start) {
+    var matches = pattern.allMatches(str)
+        .where((Match m) => m.start >= start)
+        .take(1)
+        .toList();
+
+    assert(matches.length <= 1);
+    if(matches.isEmpty){
       return null;
     }
-    return carry;
+    return matches[0];
   }
 }
 
