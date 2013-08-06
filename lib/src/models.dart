09d8c304d812a37506812dbb62e5f4954ba5052e
Kevin Moore <kevin@thinkpixellab.com>
2013-08-06 12:33:38 -0500

    starting to noodle w/ models

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -1,11 +1,31 @@
 library nv.models;
 
+import 'package:meta/meta.dart';
+
 class Note {
+  final String title;
+  final Content content;
 
+  Note(this.title, this.content) {
+    assert(title != null);
+    assert(content != null);
+  }
 
+  // TODO: hashcode and ==
+
+  @override
+  String toString() => 'Note: $title';
 }
 
 abstract class Content {
 
 }
 
+class TextContent {
+  final String value;
+
+  TextContent(this.value) {
+    assert(value != null);
+  }
+
+}
