c5096cf6e9d87a9d32683177a11b8ae0a3f4fa5c
Kevin Moore <kevin@thinkpixellab.com>
2013-08-06 17:45:16 -0500

    bootstrapping notes w/ PNP values

--- web/web_app.dart
+++ web/web_app.dart
@@ -1,12 +1,22 @@
 import 'dart:html';
 import 'package:mdv/mdv.dart' as mdv;
 import 'package:nv/nv.dart';
+import '../test/data/pride_and_prejudice.dart';
 
 void main() {
   mdv.initialize();
 
   var model = new AppModel();
 
+  _bootstrapPNP(model);
+
   query('#app').model = model;
   query('#debug').model = model;
+}
+
+void _bootstrapPNP(AppModel model) {
+
+  PNP.forEach((String chapter, String content) {
+    model.notes.add(new Note(chapter, new TextContent(content)));
+  });
 }
