cbd6514e5d764f6ca735470ea94720c0e62d9efa
Kevin Moore <kevin@thinkpixellab.com>
2013-08-10 14:44:13 -0500

    Adding NVError to shared

--- lib/src/shared.dart
+++ lib/src/shared.dart
@@ -5,3 +5,14 @@ import 'dart:collection';
 import 'package:observe/observe.dart';
 
 part 'shared/read_only_observable_list.dart';
+
+class NVError extends Error {
+  final String message;
+
+  NVError(this.message) {
+    assert(message != null);
+    assert(message.isNotEmpty);
+  }
+
+  String toString() => 'NVError: $message';
+}
