1df9daa8f791a85677bcdf74aa2ed79b9ce72d37
Kevin Moore <kevin@thinkpixellab.com>
2013-08-20 14:42:11 -0700

    save changes as notes are edited

--- lib/elements/app_element.dart
+++ lib/elements/app_element.dart
@@ -1,15 +1,19 @@
 import 'dart:html';
+import 'package:meta/meta.dart';
 import 'package:polymer/polymer.dart';
-import 'package:nv/src/controllers.dart';
-import 'package:nv/src/models.dart';
 import 'package:nv/init.dart' as init;
 import 'package:nv/elements/interfaces.dart';
+import 'package:nv/src/controllers.dart';
+import 'package:nv/src/models.dart';
+import 'package:nv/src/shared.dart';
 
 @CustomTag('app-element')
 class AppElement extends PolymerElement with ChangeNotifierMixin {
   bool get applyAuthorStyles => true;
 
+  bool _childEventsWired = false;
   AppController _controller;
+  Note _currentNote;
 
   AppController get controller => _controller;
 
@@ -21,9 +25,38 @@ class AppElement extends PolymerElement with ChangeNotifierMixin {
     });
   }
 
+  //
+  // Overrides
+  //
+
+  @override
+  void inserted() {
+    assert(!_childEventsWired);
+
+    // TODO: hold on to the subscription. Tear down at some point?
+    filterPropertyChangeRecords(_editor, const Symbol('text'))
+      .listen(_editorTextChanged);
+
+    _childEventsWired = true;
+  }
+
+  //
+  // Event handlers
+  //
+
   void handleNoteClick(Event e, var detail, Element target) {
     e.preventDefault();
     _noteClick(target.dataset['noteTitle']);
+  }
+
+  //
+  // Implementation
+  //
+
+  void _editorTextChanged(_) {
+    var title = _currentNote.title;
+    var noteContent = new TextContent(_editor.text);
+    _controller.updateNote(title, noteContent);
   }
 
   void _noteClick(String noteTitle) {
@@ -32,7 +65,12 @@ class AppElement extends PolymerElement with ChangeNotifierMixin {
   }
 
   void _loadNote(Note note) {
+    if(_currentNote != null) {
+      throw new UnimplementedError('tbd');
+    }
     assert(note.content is TextContent);
+
+    _currentNote = note;
 
     TextContent content = note.content;
 
