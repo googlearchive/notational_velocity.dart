8c4b92a4734d3aa40bb6e053942f8700e1781bed
Kevin Moore <kevin@thinkpixellab.com>
2013-08-20 12:54:14 -0700

    EditorInterface is observable

--- lib/elements/interfaces.dart
+++ lib/elements/interfaces.dart
@@ -1,9 +1,11 @@
 library nv.element_interfaces;
 
+import 'package:polymer/polymer.dart';
+
 // PolymerBug: https://github.com/GoogleChrome/notational_velocity.dart/issues/5
 // Why can't I just use the type directly?
 
-abstract class EditorInterface {
+abstract class EditorInterface implements Observable {
   String get text;
   void set text(String val);
 
