e66eaede101b364b582be99835ec8380059a7852
Kevin Moore <kevin@thinkpixellab.com>
2013-09-09 19:20:22 -0300

    make SelectionManager a subclass of MappedListView
    for now...hope to not regret it...

--- lib/src/shared/selection_manager.dart
+++ lib/src/shared/selection_manager.dart
@@ -1,7 +1,29 @@
 part of nv.shared;
 
-class SelectionManager<E> {
-  final ObservableList<E> list;
+class SelectionManager<E> extends MappedListView<E, Selectable<E>> {
+  final ObservableList<E> source;
 
-  SelectionManager(this.list);
+  E _selectedItem;
+  int _selectedIndex;
+
+  SelectionManager(ObservableList<E> source) :
+    this.source = source,
+    super(source, (e) => new Selectable<E>._(e));
+
+  E get selectedItem => _selectedItem;
+
+  int get selectedIndex => _selectedIndex;
+
+  bool get hasSelection => _selectedIndex != null;
+}
+
+class Selectable<E> extends ChangeNotifierBase {
+  final E value;
+
+  bool _isSelected = false;
+
+  Selectable._(this.value);
+
+  bool get isSelected => _isSelected;
+
 }
--- test/shared/test_selection_manager.dart
+++ test/shared/test_selection_manager.dart
@@ -5,16 +5,58 @@ import 'package:nv/src/shared.dart';
 import 'package:unittest/unittest.dart';
 
 void main() {
-  test("foo", () {
+  test("no initial selection", () {
+    SelectionManager<int> manager = _getManager();
 
+    _expectNoSelection(manager);
   });
+
+  // initially, no item is selected
+
+  // mark item selected by calling item method
+
+  // mark another item selected by calling list method
+
+  // select same item again -> no events
+
+  // remove item from source list -> watch for events
 }
 
-SelectionManager<int> _getManager([Iterable<int> source]) {
+void _expectNoSelection(SelectionManager manager) {
+  _expectAlignment(manager);
+
+  expect(manager.selectedItem, isNull);
+  expect(manager.selectedIndex, isNull);
+  expect(manager.hasSelection, isFalse);
+  expect(manager.any((s) => s.isSelected), isFalse);
+}
+
+void _expectSelection(SelectionManager manager) {
+  _expectAlignment(manager);
+
+  expect(manager.hasSelection, isTrue);
+  var index = manager.selectedIndex;
+  expect(index, isNotNull);
+  expect(index >= 0, isTrue);
+  expect(manager.selectedItem, equals(manager[index].value));
+  expect(manager[index].isSelected, isTrue);
+
+  var singleSelected = manager.singleWhere((s) => s.isSelected);
+  expect(singleSelected.value, equals(manager.selectedItem));
+}
+
+void _expectAlignment(SelectionManager manager) {
+  expect(manager.length, equals(manager.source.length));
+  for(var i = 0; i < manager.length; i++) {
+    expect(manager[i].value, equals(manager.source[i]));
+  }
+}
+
+SelectionManager<int> _getManager([Iterable<int> source = const [1,2,3,4,5]]) {
   if(source == null) {
     source = [];
   }
 
   var ol = new ObservableList.from(source);
-  return new SelectionManager<int>(source);
+  return new SelectionManager<int>(ol);
 }
