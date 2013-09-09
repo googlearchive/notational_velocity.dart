08f31a64efa5ba25f6c52a3739ed0c8d637485d3
Kevin Moore <kevin@thinkpixellab.com>
2013-09-09 14:08:49 -0300

    removed unused import

--- test/nv/test_app_controller.dart
+++ test/nv/test_app_controller.dart
@@ -7,7 +7,6 @@ import 'package:observe/observe.dart';
 import 'package:nv/src/config.dart';
 import 'package:nv/src/controllers.dart';
 import 'package:nv/src/models.dart';
-import 'package:nv/src/shared.dart';
 
 import 'package:nv/src/serialization.dart';
 import 'package:nv/src/storage.dart';
