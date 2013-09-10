1573cb7df69109654e6ba6a3b44b20c292bed260
Kevin Moore <kevin@thinkpixellab.com>
2013-09-10 14:11:30 -0300

    trivial test for MappedListView

--- test/shared/test_mapped_list_view.dart
+++ test/shared/test_mapped_list_view.dart
@@ -1,5 +1,6 @@
 library test.nv.shared.mapped_list_view;
 
+import 'dart:async';
 import 'package:observe/observe.dart';
 import 'package:nv/src/shared.dart';
 import 'package:unittest/unittest.dart';
@@ -16,6 +17,44 @@ void main() {
       test_cv.sharedMain(_simpleFactory);
     });
   });
+
+  group('core tests', () {
+    ObservableList<int> ol;
+    MappedListView<int, int> mlv;
+    StreamSubscription sub;
+    List<ChangeRecord> changes;
+
+    setUp(() {
+      ol = new ObservableList.from([1,2,3,4,5]);
+      mlv = new MappedListView<int, int>(ol, _mapper);
+      sub = mlv.changes.listen((List<ChangeRecord> val) {
+        changes = val;
+      });
+    });
+
+    tearDown(() {
+      sub.cancel();
+      sub = null;
+      changes = null;
+      ol = null;
+      mlv = null;
+    });
+
+    test('two adds', () {
+      ol.add(6);
+      ol.removeAt(0);
+
+      ol.deliverChanges();
+      mlv.deliverChanges();
+      expect(mlv, orderedEquals(ol.map(_mapper)));
+      expect(changes, hasLength(2));
+    });
+  });
+
+}
+
+int _mapper(int source) {
+  return source * 42;
 }
 
 ObservableList<int> _simpleFactory(ObservableList<int> source) {
