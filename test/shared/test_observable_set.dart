305236ce6f4c151bda9db1578e2d0183cfeba70d
Kevin Moore <kevin@thinkpixellab.com>
2013-09-18 13:59:34 -0300

    test changes from ObservableSet

--- test/shared/test_observable_set.dart
+++ test/shared/test_observable_set.dart
@@ -1,7 +1,10 @@
-library test.nv.shared.selection_manager;
+library test.nv.shared.observable_set;
 
+import 'package:observe/observe.dart';
 import 'package:nv/src/shared.dart';
 import 'package:unittest/unittest.dart';
+
+import '../src/observe_test_utils.dart';
 
 void main() {
   test('empty', () {
@@ -16,18 +19,43 @@ void main() {
 
   test('values', () {
     var set = new ObservableSet.from([1,2,3,3,2,1]);
+    List<ChangeRecord> changes;
+
+    set.changes.listen((List<ChangeRecord> val) {
+      changes = val;
+    });
 
     expect(set, orderedEquals([1,2,3]));
 
     set.add(1);
+    expect(set.deliverChanges(), isFalse);
+    expectChanges(changes, null);
 
     expect(set, orderedEquals([1,2,3]));
 
     set.addAll([3,4,5,6,1,-1]);
+    expect(set.deliverChanges(), isTrue);
+
+    expectChanges(changes, [lengthChange,
+                            change(0, addedCount: 1),
+                            change(4, addedCount: 3)]);
 
     expect(set, orderedEquals([-1,1,2,3,4,5,6]));
 
-    set.remove(5);
+    changes = null;
+
+    expect(set.remove(5), isTrue);
+    expect(set.deliverChanges(), isTrue);
+
+    expectChanges(changes, [lengthChange,
+                            change(5, removedCount: 1)]);
+
+    changes = null;
+
+    expect(set.remove(5), isFalse);
+    expect(set.deliverChanges(), isFalse);
+    expectChanges(changes, null);
+
 
     const target = const[-1,1,2,3,4,6];
     expect(set, orderedEquals(target));
