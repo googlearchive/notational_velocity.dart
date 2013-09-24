b11afe7d279a2c3e05eac45c3137c617c7e4a71f
Kevin Moore <kevin@thinkpixellab.com>
2013-09-24 19:21:05 -0300

    move core build.dart file to just handle linting

--- build.dart
+++ build.dart
@@ -1,13 +1,7 @@
 #!/usr/bin/env dart
 
-import 'dart:io';
 import 'package:polymer/builder.dart';
 
 void main() {
-  var args = new Options().arguments;
-  args.addAll(['--out', 'build']);
-
-  var options = parseOptions(args);
-
-  build(entryPoints: ['web/index.html'], options: options);
+  lint();
 }
